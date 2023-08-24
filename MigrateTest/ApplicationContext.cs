846f82cdb0ecff7cf4da290da7e40bc1642fb783
PixelBest <nick.poroschin@yandex.ru>
2023-08-24 16:13:52 +0500

    Добавьте файлы проекта.

---
+++ MigrateTest/ApplicationContext.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lims.Models;
+using System.Collections.ObjectModel;
+
+namespace Excel
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<msg> msg { get; set; }
+        public DbSet<DocumentRouteStepT> DocumentRouteStepT { get; set; }
+        public DbSet<TestOrderLineT> TestOrderLineT { get; set; }
+        public DbSet<Desc> Desc { get; set; }
+        public DbSet<BaseGuid> BaseGuid { get; set; }
+        public DbSet<Sample> Sample { get; set; }
+        public DbSet<TestOrder> TestOrder { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Excel;Username=postgres;Password=123123");
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+    }
+    [Table("BaseGuid")]
+    public class BaseGuid
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("Guid")]
+        public string? Guid { get; set; }
+        [Column("DESCRIPTION_TYPE_ID")]
+        public int? DESCRIPTION_TYPE_ID { get; set; }
+        [Column("CODE")]
+        public int? CODE { get; set; }
+        [Column("SHORT_NAME")]
+        public string? SHORT_NAME { get; set; }
+        [Column("NAME")]
+        public string? NAME { get; set; }
+        [Column("BEGIN_DATE")]
+        public string? BEGIN_DATE { get; set; }
+        [Column("END_DATE")]
+        public string? END_DATE { get; set; } 
+        [Column("NOTE")]
+        public string? NOTE { get; set; }
+        [Column("SHORT_TEST_TYPE")]
+        public string? SHORT_TEST_TYPE { get; set; }
+    }
+    public class TestOrder
+    {
+        public int Id { get; set; }
+        public string NumProtocol { get; set; }
+        public DateTime DocumentDate { get; set; }
+        public string? Type { get; set; }
+        public string? Plavka { get; set; }
+        public string? Consignment { get; set; }
+        public float D { get; set; }
+        public float S { get; set; }
+        public string? Standart { get; set; }
+        public string? Grade { get; set; }
+        public string? StrenghtGroup { get; set; }
+        public string? Laboratory { get; set; }
+        public string? Status { get; set; }
+        public string? Number { get; set; }
+
+        public ObservableCollection<Sample> Samples { get; set; }
+
+    }
+
+    public class Sample
+    {
+        public int Id { get; set; }
+        public int TestOrderId { get; set; }
+        public string? SampleNum { get; set; }
+        public string? Result { get; set; }
+        public string? TestType { get; set; }
+        public string? TestShortType { get; set; }
+        public string? EI { get; set; }
+        public string? Temperature { get; set; }
+        public string? Hardness { get; set; }
+        public string? Note { get; set; }
+        public string? SampleType { get; set; }
+        public string? Value { get; set; }
+        public bool? T_O { get; set; }
+        public bool? TestCode { get; set; }
+        public byte[]? QR { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ MigrateTest/msg.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Lims.Models
+{
+    [Serializable]
+    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Envelope
+    {
+        [XmlElement(ElementName = "Header", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public Header Header { get; set; }
+        [XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public Body Body { get; set; }
+    }
+
+    public class Header { }
+
+    public class Body
+    {
+        [XmlElement(ElementName = "ProccessMessage", Namespace = "http://tempuri.org/")]
+        public ProccessMessage ProccessMessage { get; set; }
+    }
+
+    [Serializable]
+    public class ProccessMessage
+    {
+        [XmlElement(ElementName = "msg", Namespace = "http://tempuri.org/")]
+        public msg msg { get; set; }
+    }
+
+    [Serializable]
+    public class msg
+    {
+        [Key]
+        public int? Id { get; set; }
+        [XmlElement(ElementName = "Descriptions", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public string Descriptions { get; set; }
+        [XmlElement(ElementName = "DocumentDate", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public DateTime DocumentDate { get; set; }
+        [XmlArray(ElementName = "DocumentRouteSteps", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public List<DocumentRouteStepT> DocumentRouteSteps { get; set; }
+        [XmlElement(ElementName = "DstUnitCode", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public decimal DstUnitCode { get; set; }
+        [XmlElement(ElementName = "Name", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public decimal Name { get; set; }
+        [XmlElement(ElementName = "No", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public decimal No { get; set; }
+        [XmlElement(ElementName = "TestOrderGuid", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public string TestOrderGuid { get; set; }
+        [XmlArray(ElementName = "TestOrderLines", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public List<TestOrderLineT> TestOrderLines { get; set; }
+        [XmlElement(ElementName = "UnitCode", Namespace = "http://schemas.datacontract.org/2004/07/Dipipe.IF")]
+        public decimal UnitCode { get; set; }
+
+        public msg() { }
+
+        public msg(string descriptions, DateTime documentDate, List<DocumentRouteStepT> documentRouteSteps, decimal dstUnitCode, decimal name, decimal no, string testOrderGuid, List<TestOrderLineT> testOrderLines, decimal unitCode)
+        {
+            Descriptions = descriptions;
+            DocumentDate = documentDate;
+            DocumentRouteSteps = documentRouteSteps;
+            DstUnitCode = dstUnitCode;
+            Name = name;
+            No = no;
+            TestOrderGuid = testOrderGuid;
+            TestOrderLines = testOrderLines;
+            UnitCode = unitCode;
+        }
+    }
+
+
+    [Serializable]
+    public class DocumentRouteStepT
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DocumentRouteStepGuid { get; set; }
+        public string DocumentStatus { get; set; }
+        public decimal No { get; set; }
+        public decimal OperationCode { get; set; }
+        public DateTime OperationDate { get; set; }
+        public string UnitCode { get; set; }
+        public int msgId { get; set; }
+
+        public DocumentRouteStepT() { }
+
+    }
+    [Serializable]
+    public class TestOrderLineT
+    {
+        [Key]
+        public int Id { get; set; }
+        public decimal MaxValue { get; set; }
+        public decimal MinValue { get; set; }
+        public string RefGuid { get; set; }
+        public string StringValue { get; set; }
+        public DateTime TestDate { get; set; }
+        public string TestOrderLineGuid { get; set; }
+        public string TestResultGuid { get; set; }
+        public string TestTypeGuid { get; set; }
+        public string TestTypeKindGuid { get; set; }
+        public int UnitMeas { get; set; }
+        public int msgId { get; set; }
+        public List<Desc> Descriptions { get; set; }
+
+        public TestOrderLineT() { }
+    }
+
+    [Serializable]
+    public class Desc
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime BeginDate { get; set; }
+        public string DescGuid { get; set; }
+        public string DescTypeGuid { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal MaxValue { get; set; }
+        public decimal MinValue { get; set; }
+        public string RefGuid { get; set; }
+        public string StringValue { get; set; }
+        public int UnitMeas { get; set; }
+        public int TestOrderLineTId { get; set; }
+        public int msgId { get; set; }
+
+
+        public Desc() { }
+    }
+}
+
+
